bbfd43adf62e461fd158c607d27dd09a618e9244
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-02-20 12:12:49 +0300

    list update

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import List from './List'
 
 
 const App = () => {
-
-const [people,setPeople] = useState(data)
-
+  const [people, setPeople] = useState(data)
+  
 
 const firstText = () => {
   if(people.length === 0) {
     return(
-      <h3>Görüntülenecek Tebrik Yok</h3>
+      <h3>Görüntülenecek Tebrik Tok</h3>
     )
   }else{
     return <h3>{people.length} Doğum Günü Tebriği</h3>
   }
 }
 
-
+ const removePerson =(id) =>{
+ const newList= people.filter((person) => person.id !== id)
+ setPeople(newList)
+ }
   return (
     <main>
-    <section className='container'>
-    {firstText()}
-    <List />
-    
-  <button>Tebrikleri Sil</button>
-{ people.length === 0 &&  <button>Tebrikleri Yenile</button>}
-    </section>
+      <section className='container'>
+       {firstText()}
+        <List people={people} removePerson={removePerson} />
+        <button onClick={() => setPeople([])}>Tebrikleri Sil</button>
+         <button onClick={() =>setPeople(data)} > Tebrikleri Göster</button>
+      </section>
     </main>
   )
 }
--- src/List.js
+++ src/List.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const List = () => {
+const List = ({people}) => {
   return (
     <div>
-      List
+      {people.map((person) => {
+        const {id,name,image,age} = person
+
+        return(
+          <article key={id} className='person'>
+          <img src={image} alt={name}/>
+
+          <div>
+            <h4>{name}</h4>
+            <p>{age} Yaşında</p>
+          </div>
+
+           <button className='remove-btn'> Tebriği Sil </button>
+
+          </article>  
+        )
+      })}
     </div>
   )
 }
