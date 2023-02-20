2836147d8bf7a0943b26b95d718db9826f992ad3
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-02-20 12:00:41 +0300

    App.js update and List.js create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+const [people,setPeople] = useState(data)
+
+
+const firstText = () => {
+  if(people.length === 0) {
+    return(
+      <h3>Görüntülenecek Tebrik Yok</h3>
+    )
+  }else{
+    return <h3>{people.length} Doğum Günü Tebriği</h3>
+  }
 }
 
-export default App;
+
+  return (
+    <main>
+    <section className='container'>
+    {firstText()}
+    <List />
+    
+  <button>Tebrikleri Sil</button>
+{ people.length === 0 &&  <button>Tebrikleri Yenile</button>}
+    </section>
+    </main>
+  )
+}
+
+export default App
